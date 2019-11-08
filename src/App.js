cca13d37eed113d230f1db2efdcfc49479eb1edc
Mark Romano <markromano@Marks-MBP.lan1>
2019-11-08 08:41:59 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 2
+    }
+    this.increment = this.increment.bind(this)
+  }
+
+  increment() {
+    const newCount = this.state.count + 1;
+    this.setState({ count: newCount });
+  }
+
+  render() {
+    const count = this.state.count
+    return (
+      <div>
+        <Header headerText={'bananas ' + count} increment={this.increment}/>
+        <Header headerText={'cherries ' + count} increment={this.increment}/>
+      </div>
+    );
+  }
 }
 
 export default App;
